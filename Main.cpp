288aa034e9c63107550ed93c6c6d39f0ada2c65d
Anthony Pasche <anthony.pasche@gmail.com>
2017-07-01 16:42:56 +0200

    Refactoring code & add glewInit

--- Main.cpp
+++ Main.cpp
@@ -5,17 +5,38 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-struct Shader
+class Shader
 {
-	static GLint Attach(GLuint program, GLuint shader)
+public:
+	static GLuint id;
+	
+	static bool Set(const char *vertex, const char *geometry, const char *fragment)
 	{
-		if (shader > 0) glAttachShader(program, shader);
+		GLuint loaded = 0;
+		if (vertex) loaded += Load(GL_VERTEX_SHADER, vertex);
+		if (geometry) loaded += Load(GL_GEOMETRY_SHADER, geometry);
+		if (fragment) loaded += Load(GL_FRAGMENT_SHADER, fragment);
+		if (loaded == 0) return false;
+		
+		if (id > 0) Delete();
+		id = glCreateProgram();
+		if (id == 0) return false;
+		glLinkProgram(id);
+		GLint linked;
+		glGetProgramiv(id, GL_LINK_STATUS, &linked);
+		return linked == GL_TRUE;
 	}
 	
-	static GLint Load(GLenum type, const char *file)
+	static void Delete()
+	{
+		if (id > 0) glDeleteProgram(id);
+	}
+	
+private:
+	static bool Load(GLenum type, const char *file)
 	{
 		GLuint shader = glCreateShader(type);
-		if (shader == 0) return -1;
+		if (shader == 0) return false;
 		
 		FILE *in = fopen(file, "rb");
 		fseek(in, 0, SEEK_END);
@@ -31,58 +52,89 @@ struct Shader
 		glCompileShader(shader);
 		GLint compiled;
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-		if (compiled == GL_FALSE) return -2;
-		return shader;
-	}
-	
-	static GLint Create(GLuint vertex, GLuint geometry, GLuint fragment)
-	{
-		GLint program = glCreateProgram();
-		if (program == 0) return -1;
-		Attach(program, vertex);
-		Attach(program, geometry);
-		Attach(program, fragment);
-		glLinkProgram(program);
-		GLint linked;
-		glGetProgramiv(program, GL_LINK_STATUS, &linked);
-		return linked == GL_FALSE ? -2 : program;
-	}
-	
-	static void Delete(GLuint program)
-	{
-		glUseProgram(0);
-		glDeleteProgram(program);
+		if (compiled == GL_FALSE) return false;
+		glAttachShader(id, shader);
+		return true;
 	}
 };
 
-struct FrameBuffer
+GLuint Shader::id = 0;
+
+class PostProcess
 {
-	static GLint Create()
+public:
+	static GLuint texture;
+	static GLuint frameBuffer;
+	static GLuint renderBuffer;
+	
+	static bool Set()
 	{
+		printf("-1\n");
+		if (texture > 0) Delete();
+		
+		printf("-2\n");
+		glGenTextures(1, &texture);
+		if (texture == 0) return false;
+		
+		printf("-3\n");
+		glGenRenderbuffers(1, &renderBuffer);
+		if (renderBuffer == 0) return false;
+		
+		glGenFramebuffers(1, &frameBuffer);
+		if (frameBuffer == 0) return false;
+		
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+		
+		glBindRenderbuffer(GL_RENDERBUFFER, renderBuffer);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, WIDTH, HEIGHT);
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBuffer);
+		return true;
 	}
 	
 	static void Delete()
 	{
+		if (renderBuffer > 0) glDeleteRenderbuffers(1, &renderBuffer);
+		if (texture > 0) glDeleteTextures(1, &texture);
+		if (frameBuffer > 0) glDeleteFramebuffers(1, &frameBuffer);
 	}
 };
 
-struct VertexBuffer
+GLuint PostProcess::texture = 0;
+GLuint PostProcess::frameBuffer = 0;
+GLuint PostProcess::renderBuffer = 0;
+
+class VertexBuffer
 {
-	static GLint Create(GLsizeiptr size, GLvoid *data, GLenum usage)
+public:
+	static GLuint id;
+
+	static bool Set(GLsizeiptr size, GLvoid *data, GLenum usage)
 	{
-		GLuint buffer;
-		glGenBuffers(1, &buffer);
-		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		if (data == NULL) return false;
+		if (id > 0) Delete();
+		
+		glGenBuffers(1, &id);
+		if (id == 0) return false;
+		
+		glBindBuffer(GL_ARRAY_BUFFER, id);
 		glBufferData(GL_ARRAY_BUFFER, size, data, usage);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		return true;
 	}
 	
-	static void Delete(GLuint buffer)
+	static void Delete()
 	{
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDeleteBuffers(1, &buffer);
+		if (id > 0) glDeleteBuffers(1, &id);
 	}
 };
+
+GLuint VertexBuffer::id = 0;
 
 struct App
 {
@@ -91,6 +143,10 @@ struct App
 	
 	static int Shutdown(int exit)
 	{
+		VertexBuffer::Delete();
+		PostProcess::Delete();
+		Shader::Delete();
+		
 		if (gl) SDL_GL_DeleteContext(gl);
 		if (window) SDL_DestroyWindow(window);
 		SDL_Quit();
@@ -106,7 +162,13 @@ struct App
 		if (window == NULL) return Shutdown(2);
 		
 		gl = SDL_GL_CreateContext(window);
-		if (gl == NULL) return Shutdown(3);
+		if (gl == NULL || glewInit() != GLEW_OK) return Shutdown(3);
+		
+		if (!Shader::Set(NULL, NULL, NULL)) return Shutdown(4);
+		if (!PostProcess::Set()) return Shutdown(5);
+		if (!VertexBuffer::Set(0, NULL, 0)) return Shutdown(6);
+		
+		printf("Initialized\n");
 		
 		return 0;
 	}
